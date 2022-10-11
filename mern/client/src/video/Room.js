e96ac2ed6fd5f80287c5c9effc6d3433a2ed4863
ASIF-Mahmud1 <1421015@iub.edu.bd>
2022-10-11 16:18:23 +0600

    [add] twilio video code shifted in this repository

---
+++ mern/client/src/video/Room.js
@@ -0,0 +1,99 @@
+import React, { useEffect, useState } from "react";
+import Participant from "./Participant";
+
+const Room = ({ roomName, room, handleLogout }) => {
+  const [participants, setParticipants] = useState([]);
+  const [videoEnabled, setVideo]= useState(true)
+  const [audioEnabled, setAudio]= useState(true)
+
+  useEffect(() => {
+    const participantConnected = (participant) => {
+      setParticipants((prevParticipants) => [...prevParticipants, participant]);
+    };
+
+    const participantDisconnected = (participant) => {
+      setParticipants((prevParticipants) =>
+        prevParticipants.filter((p) => p !== participant)
+      );
+    };
+
+    room.on("participantConnected", participantConnected);
+    room.on("participantDisconnected", participantDisconnected);
+    room.participants.forEach(participantConnected);
+    return () => {
+      room.off("participantConnected", participantConnected);
+      room.off("participantDisconnected", participantDisconnected);
+    };
+  }, [room]);
+
+  const remoteParticipants = participants.map((participant) => (
+    <Participant key={participant.sid} participant={participant} />
+  ));
+
+  const handleAudio=()=>{
+    if(audioEnabled)
+    {
+      room.localParticipant.audioTracks.forEach(publication => {
+        publication.track.disable();
+      });
+      
+      setAudio(prevState=> !prevState)
+    }
+    else 
+    {
+      room.localParticipant.audioTracks.forEach(publication => {
+        publication.track.enable();
+      });
+      
+      setAudio(prevState=> !prevState)
+    }
+  }
+  const handleVideo=()=>{
+    if(videoEnabled)
+    {
+      room.localParticipant.videoTracks.forEach(publication => {
+        publication.track.disable();
+      });
+      setVideo(prevState=> !prevState)
+    }
+    else 
+    {
+      room.localParticipant.videoTracks.forEach(publication => {
+        publication.track.enable();
+      });
+      setVideo(prevState=> !prevState)
+    }
+
+  }
+  return (
+    <>
+        <div style={{display:'flex', flexDirection:'column',borderWidth:1, borderColor:'red'}}>
+       <br/>
+       <button onClick={handleAudio}>Enable Audio</button><br/>
+      <button onClick={handleVideo}>Enable Video</button>
+   
+       <br/>
+      </div>
+    <div className="room">
+     
+      <h2>Room: {roomName}</h2>
+      <button onClick={handleLogout}>Log out</button>
+  
+      <div className="local-participant">
+        {room ? (
+          <Participant
+          key={room.localParticipant.sid}
+          participant={room.localParticipant}
+          />
+          ) : (
+            ""
+            )}
+      </div>
+      <h3>Remote Participants</h3>
+      <div className="remote-participants">{remoteParticipants}</div>
+    </div>
+            </>
+  );
+};
+
+export default Room;
