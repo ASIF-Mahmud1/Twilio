7aad46cd756d7866b4f4505fd371a377051b8353
ASIF-Mahmud1 <1421015@iub.edu.bd>
2022-11-17 13:20:23 +0600

    [edit] running in local machine

--- mern/client/src/config/config.js
+++ mern/client/src/config/config.js
@@ -1,5 +1,5 @@
-// const url= "http://localhost:5000/" 
-const url= "https://twilio-serve-anati4c1a-asif-mahmud1.vercel.app/" 
+const url= "http://localhost:5000/" 
+//const url= "https://twilio-serve-anati4c1a-asif-mahmud1.vercel.app/" 
 
 export{
     url
