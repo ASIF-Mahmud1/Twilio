4e380798a6cbebae56c783b5e38ddd4954a4b0ff
ASIF-Mahmud1 <1421015@iub.edu.bd>
2022-12-26 19:18:57 +0600

    use live url

--- mern/client/src/config/config.js
+++ mern/client/src/config/config.js
@@ -1,5 +1,5 @@
-const url= "http://localhost:5000/" 
-//const url= "https://twilio-serve.vercel.app/" 
+//const url= "http://localhost:5000/" 
+const url= "https://twilio-serve.vercel.app/" 
 
 export{
     url
